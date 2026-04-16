13ecdebadd30983f9d26474b45c06b52705b26c9
shmakadyavka <englishmanaman@gmail.com>
2026-04-16 20:59:14 +0600

    update nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Running migrations..."
-python manage.py migrate
+# Check if migrations are needed
+echo "Checking database status..."
+python manage.py showmigrations --plan | grep -q "\[ \]" && {
+    echo "Running migrations..."
+    python manage.py migrate
+} || echo "No migrations needed."
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
